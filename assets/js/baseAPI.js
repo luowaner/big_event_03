8998f5565c567034f3fe7bbcc44429cac5a654a2
seven7 <1450416324@qq.com>
2021-03-16 01:24:32 +0800

    index done

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -7,5 +7,25 @@ let baseURL = 'http://api-breakingnews-web.itheima.net'
 
 // $.ajaxPrefilter可以在调用$.get()$.post()$.ajax()方法之后会立即触发此事件,接收到ajax响应后,还会触发此方法
 $.ajaxPrefilter(function (options) {
-    options.url = baseURL + options.url
+    // 1添加根路径
+    options.url = baseURL + options.url;
+    // 2身份认证
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    };
+
+    // 3登录拦截
+    options.complete = function (res) {
+        console.log(res.responseJSON);
+        let obj = res.responseJSON;
+        if (obj.status !== 0 && obj.message == '身份认证失败！') {
+            // 清空本地token
+            localStorage.removeItem('token');
+            // 跳转到登录页面
+            location.href = '/login.html'
+        }
+
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,51 @@
+$(function () {
+    // 获取用户信息
+    getUserInfo();
+    // 退出功能
+    let layer = layui.layer;
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确定退出吗?', { icon: 3, title: '提示' }, function (index) {
+            //清空本地token
+            localStorage.removeItem('token');
+            // 跳转登录页面
+            location.href = '/login.html';
+            // 关闭询问框
+            layer.close(index);
+        });
+    });
+
+    
+});
+// 封装:获取用户信息函数
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: (res) => {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg(res.massage);
+            }
+            // 请求成功,渲染头像
+            renderAvatar(res.data)
+        }
+    });
+}
+
+// 封装渲染头像函数
+function renderAvatar(user) {
+    console.log(user);
+    // 获取用户信息
+    let name = user.nickname || user.username;
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    // 判定
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').show().attr('src', user.user_pic);
+        $('.text-avatar').hide();
+    } else {
+        $('.layui-nav-img').hide();
+        $('.text-avatar').show().html(name[0].toUpperCase())
+    }
+}
